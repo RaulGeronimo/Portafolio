60b07b52bc70938bc4ff46bf5d97b999d05424f7
Raul Geronimo <raul090700@gmai.com>
2023-02-19 22:46:46 -0600

    Repertorio

--- doc/CRUD/Musica Angular/MusicaAngular.sql
+++ doc/CRUD/Musica Angular/MusicaAngular.sql
@@ -28,7 +28,7 @@ CREATE TABLE `album` (
   `idDisquera` int(11) DEFAULT NULL,
   `Nombre` varchar(60) DEFAULT NULL,
   `Duracion` time DEFAULT NULL,
-  `Lanzamiento` date DEFAULT NULL,
+  `Lanzamiento` varchar(50) DEFAULT NULL,
   `Grabacion` varchar(200) DEFAULT NULL,
   `Genero` varchar(100) DEFAULT NULL,
   `Portada` text DEFAULT NULL,
@@ -61,8 +61,8 @@ CREATE TABLE `artista` (
   `Nombre` varchar(70) DEFAULT NULL,
   `NombreArtistico` varchar(50) DEFAULT NULL,
   `Genero` char(1) DEFAULT NULL,
-  `FechaNacimiento` date DEFAULT NULL,
-  `FechaFinado` date DEFAULT NULL,
+  `FechaNacimiento` varchar(50) DEFAULT NULL, 
+  `FechaFinado` varchar(50) DEFAULT NULL, 
   `Estatura` double DEFAULT NULL,
   `idNacionalidad` int(11) DEFAULT NULL,
   `Instrumentos` varchar(100) DEFAULT NULL,
@@ -94,8 +94,8 @@ CREATE TABLE `artista_grupo` (
   `Codigo` int(11) NOT NULL AUTO_INCREMENT,
   `idArtista` int(11) DEFAULT NULL,
   `idGrupo` int(11) DEFAULT NULL,
-  `FechaInicio` date DEFAULT NULL,
-  `FechaFin` date DEFAULT NULL,
+  `FechaInicio` varchar(50) DEFAULT NULL,
+  `FechaFin` varchar(50) DEFAULT NULL,
   `idInstrumento` int(11) DEFAULT NULL,
   PRIMARY KEY (`Codigo`),
   KEY `idArtista` (`idArtista`),
@@ -127,7 +127,7 @@ CREATE TABLE `canciones` (
   `idCancion` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(70) DEFAULT NULL,
   `Duracion` time DEFAULT NULL,
-  `Publicacion` date DEFAULT NULL,
+  `Publicacion` varchar(50) DEFAULT NULL,
   `Genero` varchar(100) DEFAULT NULL,
   `Idioma` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`idCancion`)
@@ -182,7 +182,7 @@ DROP TABLE IF EXISTS `disquera`;
 CREATE TABLE `disquera` (
   `idDisquera` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(60) DEFAULT NULL,
-  `Fundacion` date DEFAULT NULL,
+  `Fundacion` varchar(50) DEFAULT NULL,
   `Fundador` varchar(100) DEFAULT NULL,
   `Generos` varchar(100) DEFAULT NULL,
   `idPais` int(11) DEFAULT NULL,
@@ -214,8 +214,8 @@ CREATE TABLE `grupo` (
   `Nombre` varchar(50) DEFAULT NULL,
   `Origen` varchar(200) DEFAULT NULL,
   `Genero` varchar(200) DEFAULT NULL,
-  `Inicio` date DEFAULT NULL,
-  `Fin` date DEFAULT NULL,
+  `Inicio` varchar(50) DEFAULT NULL,
+  `Fin` varchar(50) DEFAULT NULL,
   `Sellos` varchar(150) DEFAULT NULL,
   `Estado` varchar(50) DEFAULT NULL,
   `SitioWeb` varchar(50) DEFAULT NULL,
@@ -613,37 +613,133 @@ SET character_set_client = @saved_cs_client;
 
 /* PROCEDIMIENTOS */
 /* ------------------------------------ ALBUM POR GRUPO ------------------------------------------ */
-DELIMITER $$
+/* DELIMITER $$
 CREATE PROCEDURE `obtener_album`(IN idGrupoB INT)
 BEGIN
 SELECT * FROM Vista_Album WHERE idGrupo = idGrupoB;
 END$$
 
-DELIMITER ;
+DELIMITER ; */
 
 /* ------------------ CANCIONES POR ALBUM --------------- */
-DELIMITER $$
+/* DELIMITER $$
 CREATE PROCEDURE `obtener_cancionesAlbum`(IN idAlbumA INT)
 BEGIN
 SELECT * FROM Vista_CancionesAlbum WHERE idAlbum = idAlbumA;
 END$$
 
-DELIMITER ;
+DELIMITER ; */
 
 /* ------------------------------------------------ CANCIONES POR GRUPO ----------------------------------------- */
-DELIMITER $$
+/* DELIMITER $$
 CREATE PROCEDURE `obtener_canciones`(IN idGrupoB INT)
 BEGIN
 SELECT * FROM Vista_CancionesGrupo WHERE idGrupo = idGrupoB;
 END$$
 
-DELIMITER ;
+DELIMITER ; */
 
 /* ------------------------------------------------ ARTISTA POR GRUPO -------------------------------------------------*/
-DELIMITER $$
+/* DELIMITER $$
 CREATE PROCEDURE `obtener_integrantes`(IN idGrupoB INT)
 BEGIN
 SELECT * FROM Vista_GrupoIntegrantes WHERE idGrupo = idGrupoB;
 END$$
 
-DELIMITER ;
+DELIMITER ; */
+
+/* DATOS */
+INSERT INTO Pais VALUES 
+(1,'Alemania','Aleman, Alemana','Europa','https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/640px-Flag_of_Germany.svg.png'),
+(2,'Mexico','Mexicano, Mexicana','America','https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/1280px-Flag_of_Mexico.svg.png'),
+(3,'Australia','Australiano, Australiana','Oceania','https://upload.wikimedia.org/wikipedia/commons/8/88/Flag_of_Australia_%28converted%29.svg'),
+(4,'Inglaterra','Ingles, Inglesa','Europa','https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Flag_of_England.svg/1920px-Flag_of_England.svg.png'),
+(5,'Estados Unidos','Americano, Americana','America','https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png'),
+(6,'Francia','Frances, Francesa','Europa','https://m.media-amazon.com/images/I/41Dka5ibTAL.jpg'),
+(7,'China','Chino, China','Asia','https://genexus.blog/wp-content/uploads/2021/09/Bandera-China.jpeg'),
+(8,'Suecia','Sueco, Sueca','Europa','https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Flag_of_Sweden.svg/1200px-Flag_of_Sweden.svg.png'),
+(9,'Rusia','Ruso, Rusa','Europa','https://mf.b37mrtl.ru/rbthmedia/images/2021.06/article/60c0e15285600a1fb24d33f7.jpg'),
+(10,'Canada','Canadiense','America','https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/800px-Flag_of_Canada_%28Pantone%29.svg.png'),
+(11,'Argentina','Argentino','America','https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/1200px-Flag_of_Argentina.svg.png'),
+(12,'Japon','Japones, Japonesa','Asia','https://setopen.sportdata.org/wkfranking/images/flags/JPN.gif'),
+(13,'Austria','Austriaco, Austriaca','Europa','https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Flag_of_Austria.svg/640px-Flag_of_Austria.svg.png'),
+(14,'Belgica','Belga','Europa','https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Flag_of_Belgium_%28civil%29.svg/1200px-Flag_of_Belgium_%28civil%29.svg.png'),
+(15,'Irlanda del Norte','Norirlandes, Norirlandesa','Europa','https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Flag_of_Northern_Ireland_%281953%E2%80%931972%29.svg/1920px-Flag_of_Northern_Ireland_%281953%E2%80%931972%29.svg.png'),
+(16,'Nueva Zelanda','Neozelandes, Neozelandesa','Oceania','https://upload.wikimedia.org/wikipedia/commons/3/3e/Flag_of_New_Zealand.svg');
+
+INSERT INTO Instrumento VALUES
+(NULL, 'Guitarra Acústica', 'La guitarra es un instrumento musical de la familia de los cordófonos, es decir los instrumentos que producen su sonido al hacer vibrar las cuerdas.', 'https://www.pngplay.com/wp-content/uploads/6/Real-Black-Acoustic-Guitar-PNG.png'),
+(NULL, 'Guitarra Eléctrica', 'La guitarra eléctrica es un tipo de guitarra en la que, a diferencia de la guitarra española y la guitarra sajona, su caja no hace resonancia, por lo que el sonido se produce indirectamente por inducción eléctrica y a través de un amplificador o también a través de un pedal de efectos.', 'https://www.pngall.com/wp-content/uploads/12/Electric-Guitar-Instrument-No-Background.png'),
+(NULL, 'Batería', 'La batería es un conjunto de instrumentos musicales de percusión usado por muchas agrupaciones musicales.', 'https://assets.stickpng.com/images/585699304f6ae202fedf274b.png'),
+(NULL, 'Bajo Eléctrico', 'El bajo, bajo eléctrico o simplemente bajo, es el miembro más grave de la familia de las guitarras. Es un instrumento de cuerda pulsada similar en apariencia y construcción a una guitarra eléctrica o acústica, pero con un mástil y una escala más largos, y típicamente de cuatro a seis cuerdas o cursos.', 'https://i.pinimg.com/originals/96/d8/20/96d820eec771a2e48c3a55cf1f70e7e3.png'),
+(NULL, 'Ninguno / Vocalista', 'El micrófono es un transductor acústico eléctrico, es decir, transforma la señal acústica de nuestra voz en la señal eléctrica que reproduce el equipo al que se encuentra conectado. Los que utilizan el micrófono son los vocalistas de un grupo de Música, ya que no tocan algún instrumento musical.', 'https://www.pngplay.com/wp-content/uploads/9/Microphone-No-Background.png');
+
+INSERT INTO Artista VALUES
+(NULL, 'Kurt Donald Cobain', 'Kurt Cobain', 'H', '1967-02-20', '1994-04-05', 1.75, 5, 'Voz, Guitarra', 'Barítono ligero', 'https://www.teahub.io/photos/full/59-596752_kurt-cobain-grunge-aesthetic.jpg'),
+(NULL, 'David Eric Grohl', 'Dave Grohl', 'H', '1969-01-14', NULL, 1.83, 5, 'Voz, Batería, Guitarra, Bajo eléctrico, Piano, Sintetizador', 'Tenor ligero', 'https://pbs.twimg.com/media/DWkk6uFUMAEMO98.jpg'),
+(NULL, 'Billie Joe Armstrong', 'Billie Joe Armstrong', 'H', '1972-02-17', NULL, 1.7, 5, 'Voz, guitarra, bajo, piano, mandolina, batería, armónica','Tenor', 'https://e0.pxfuel.com/wallpapers/175/867/desktop-wallpaper-billie-joe-armstrong-thumbnail.jpg'),
+(NULL, 'Chester Charles Bennington', 'Chester Bennington', 'H', '1973-03-20', '2017-07-20', 1.78, 5, 'Voz, guitarra, instrumento de percusión y piano', 'Tenor ligero, Gutural', 'https://i.pinimg.com/originals/5f/2a/27/5f2a2705a92355210a0c959ef44baed5.jpg');
+
+INSERT INTO Grupo VALUES
+(NULL, 'Nirvana', 'Aberdeen, Washington, Estados Unidos', 'Grunge, rock alternativo', '1987-10-03', '1994-04-05', 'Sub Pop, Tupelo Records, DGC Records, Geffen Records', 'Disuelto', 'http://nirvana.com/', 'Ingles', 'https://cdn.freebiesupply.com/logos/large/2x/nirvana-logo-png-transparent.png'),
+(NULL, 'Foo Fighters', 'Seattle, Washington, Estados Unidos', 'Hard rock, Post-grunge, Rock alternativo', '1994-07-16', NULL, 'RCA Records, Capitol Records, Roswell Records', 'Activo', 'http://foofighters.com/', 'Ingles', 'https://seeklogo.com/images/F/Foo_Fighters-logo-A7966CB57F-seeklogo.com.png'),
+(NULL, 'Green Day', 'Berkeley, California, Estados Unidos', 'Pop punk, Punk rock, Rock alternativo', '1986-10-12', NULL, 'Lookout Records, Reprise Records, Warner Bros Records', 'Activo', 'http://www.greenday.com/', 'Ingles', 'https://www.pngkey.com/png/full/230-2301807_green-day-png-vector-royalty-free-library-green.png'),
+(NULL, 'Linkin Park', 'Agoura Hills, California, Estados Unidos', '	Nu metal, rock alternativo, rap metal, metal alternativo, heavy metal, rock electrónico, pop', '1996-08-07', '2017-07-20', 'Warner Music Group, Warner Records, Machine Shop Recordings', 'En hiato', 'http://www.linkinpark.com/', 'Ingles', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/LPLogo-black.svg/1200px-LPLogo-black.svg.png');
+
+INSERT INTO Artista_Grupo VALUES
+(NULL, 1, 1, '1987-10-3', '1994-04-05', 2),
+(NULL, 2, 1, '1990-7-17', '1994-04-05', 3),
+(NULL, 2, 2, '1994-07-16', NULL, 2),
+(NULL, 3, 3, '1986-10-12', NULL, 2),
+(NULL, 4, 4, '1996-8-7', '2017-07-20', 5);
+
+INSERT INTO Disquera VALUES
+(NULL, 'DGC Records', '1990-03-20', 'David Geffen', 'Rock alternativo', 5, 'https://upload.wikimedia.org/wikipedia/en/f/fa/DGC_Records_logo%2C_1990.png'),
+(NULL, 'Sub Pop', '1986-7-16', 'Bruce Pavitt, Jonathan Poneman', 'Hip-hop, Indie pop, Punk rock, Heavy metal, Rock alternativo, Música de comedia', 5, 'https://cdn.freebiesupply.com/logos/large/2x/sub-pop-logo-png-transparent.png'),
+(NULL, 'RCA Records', '1990-01-09', 'Emile Berliner, Eldridge R. Johnson', 'Varios géneros', 5, 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/RCA_Records_%28logo%29.svg/1280px-RCA_Records_%28logo%29.svg.png'),
+(NULL, 'Warner Records', '1958-03-19', 'James Conkling', 'Varios', 5, 'https://variety.com/wp-content/uploads/2016/02/warner-records.jpg');
+
+INSERT INTO Album VALUES
+(NULL, 1, 2, 'Bleach', '00:42:14', '1989-06-15', '1988-1989', 'Grunge', 'https://m.media-amazon.com/images/I/71ThvtmT-FL._AC_SL1417_.jpg'),
+(NULL, 1, 1, 'Nevermind', '00:42:38', '1991-09-24', 'Mayo-junio de 1991', 'Grunge, rock alternativo', 'https://m.media-amazon.com/images/I/71DQrKpImPL._AC_SL1400_.jpg'),
+
+(NULL, 2, 3, 'Wasting Light', '00:47:56', '2011-04-12', '6 de septiembre de 2010 - 11 de diciembre de 2010', 'Post-grunge, hard rock y rock alternativo', 'https://m.media-amazon.com/images/I/81bcusKozsL._AC_SL1500_.jpg'),
+
+(NULL, 3, 4, 'American Idiot', '00:57:12', '2004-09-21', '18 de abril de 2003-26 de marzo de 2004 en Studio 880, Oakland, California; Ocean Way Recording, Hollywood', 'Pop punk, Punk rock, Rock alternativo, Ópera rock', 'https://m.media-amazon.com/images/I/71Z0rLIvpuL._AC_SL1448_.jpg'),
+(NULL, 3, 4, 'Revolution Radio', '00:44:29', '2016-11-11', 'noviembre de 2015 - junio de 2016 OTIS, Oakland', 'Pop punk, rock alternativo, punk rock', 'https://m.media-amazon.com/images/I/81+vo6G38sL._AC_SL1500_.jpg'),
+
+(NULL, 4, 4, 'Minutes to Midnight', '00:43:23', '2007-05-14', 'Enero de 2006 — marzo de 2007', 'Rock alternativo, metal alternativo', 'https://m.media-amazon.com/images/I/91esTaFkpPL._AC_SL1500_.jpg'),
+(NULL, 4, 4, 'Hybrid Theory', '00:37:48', '2000-10-24', 'Marzo-junio de 2000', 'Nu metal, rap metal, metal alternativo, rap rock', 'https://m.media-amazon.com/images/I/81iC+O0ec2L._AC_SL1448_.jpg'),
+(NULL, 4, 4, 'One More Light', '00:35:19', '2017-05-19', 'Noviembre de 2015-febrero de 2017', 'Pop, pop rock, electropop , Rock electrónico', 'https://ocdn.eu/images/pulscms/YzY7MDA_/85d9ab2d8707de5ea7705a70e4bb8727.jpeg');
+
+INSERT INTO Canciones VALUES
+(NULL, 'Come As You Are', '00:03:38', '1992-03-03', 'Grunge, rock alternativo', 'Inglés'),
+(NULL, 'Smells Like Teen Spirit', '00:05:01', '1991-09-10', 'Grunge, rock alternativo, hard rock', 'Inglés'),
+(NULL, 'About a Girl', '00:02:49', '1989-10-24', 'Rock alternativo', 'Inglés'),
+
+(NULL, 'Walk ', '00:04:19', '2011-06-06', '	Rock alternativo, post-grunge y hard rock', 'Inglés'),
+
+(NULL, 'American Idiot', '00:02:54', '2004-08-31', 'Pop punk', 'Inglés'),
+(NULL, 'Whatsername', '00:04:12', '2004-09-20', 'Rock alternativo', 'Inglés'),
+
+(NULL, 'Given up', '00:03:09', '2008-03-03', 'Metal alternativo, punk rock, hardcore punk, Post-Hardcore', 'Inglés'),
+(NULL, 'What I''ve Done', '00:03:27', '2007-04-02', 'Rock alternativo, Hard rock', 'Inglés'),
+(NULL, 'In the End', '00:03:37', '2001-10-09', 'Nu metal, rap rock, rock alternativo', 'Inglés'),
+(NULL, 'One More Light', '00:04:15', '2017-10-03', 'Rock electrónico, balada', 'Inglés');
+
+INSERT INTO Canciones_Album VALUES
+(NULL, 2, 1,3),
+(NULL, 2, 2,1),
+(NULL, 1, 3,3),
+
+(NULL, 3, 4,11),
+
+(NULL, 4, 5,1),
+(NULL, 4, 6,9),
+
+(NULL, 6, 7,2),
+(NULL, 6, 8,6),
+(NULL, 7, 9,8),
+(NULL, 8, 10,9);
+
+/* INSERT INTO Artista SET `idArtista` = 0, `Nombre` = 'Matthew James Bellamy', `NombreArtistico` = 'Matt Bellamy', `Genero` = 'H', `FechaNacimiento` = '1978-06-09', `FechaFinado` = null, `Estatura` = 1.7, `idNacionalidad` = '5', `Instrumentos` = 'Voz, guitarra, piano, teclados, bajo', `TipoVoz` = 'Tenor dramático', `Foto` = 'https://i.pinimg.com/originals/cf/d4/45/cfd445baa399eb6238f07c58b0e3000b.png' */
